02517f93fc285b37b35f886a6a22f10ed91aea7f
SaneleGumede <iamsanele.gumede@gmail.com>
2026-07-12 17:35:47 +0200

    Add expressions script with arithmetic operations and student data

---
+++ expressions.py
@@ -0,0 +1,27 @@
+#Question 1
+num1 = 10
+num2 = 5
+
+result = num1 * num2
+
+print(result)
+
+#Question 2
+city = "Polokwane"
+
+print("Welcome to " + city)
+
+#Question 3
+student = {
+    "name": "Lerato",
+    "marks": 75
+}
+
+print(student["name"] + " scored " + str(student["marks"]) + " marks")
+
+#Question 5
+numbers = [10, 20, 30]
+
+total = numbers[0] + numbers[1] + numbers[2]
+
+print(total)
