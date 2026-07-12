eb76a1dce35cf3b5a8e3b37925d17821aa5fa2b6
SaneleGumede <iamsanele.gumede@gmail.com>
2026-07-12 16:53:50 +0200

    Add student and vehicle dictionaries with sample data

---
+++ assign_objects.py
@@ -0,0 +1,18 @@
+#Question 1
+student = {
+    "name": "Sanele Gumede",
+    "studentNumber": "123456",
+    "course": "Data Science",
+    "yearLevel": 1
+}
+
+print(student)
+
+#Question 2
+vehicle = {}
+
+vehicle["brand"] = "Toyota"
+vehicle["colour"] = "White"
+vehicle["registrationNumber"] = "ABC123GP"
+
+print(vehicle)
